8adc9b6a6c587196256c6fcbedf5186029bd9ee2
Reggie Bingham <reggiebing11@gmail.com>
2017-07-08 16:45:53 -0700

    first commit

---
+++ ngApp/app.ts
@@ -0,0 +1,43 @@
+
+namespace claims {
+
+    angular.module('claims', ['ui.router', 'ngResource', 'ui.bootstrap']).config((
+        $stateProvider: ng.ui.IStateProvider,
+        $urlRouterProvider: ng.ui.IUrlRouterProvider,
+        $locationProvider: ng.ILocationProvider
+    ) => {
+        // Define routes
+        $stateProvider
+            .state('login', {
+                url: '/',
+                templateUrl: '/ngApp/views/login.html',
+                controller: claims.Controllers.LoginController,
+                controllerAs: 'vm'
+            })
+            .state('register', {
+                url: '/register',
+                templateUrl: '/ngApp/views/register.html',
+                controller: claims.Controllers.RegisterController,
+                controllerAs: 'vm'
+            })
+            .state('home', {
+                url: '/home',
+                templateUrl: '/ngApp/views/home.html',
+                controller: claims.Controllers.HomeController,
+                controllerAs: 'vm'
+            })
+            .state('notFound', {
+                url: '/notFound',
+                templateUrl: '/ngApp/views/notFound.html'
+            });
+
+        // Handle request for non-existent route
+        $urlRouterProvider.otherwise('/notFound');
+
+        // Enable HTML5 navigation
+        $locationProvider.html5Mode(true);
+    });
+
+
+
+}
